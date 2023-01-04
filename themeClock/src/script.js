784b361dae78bd3d4987b2c4a046aa817d7ec5eb
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-01-04 08:54:57 -0300

    feat: theme clock project

--- themeClock/src/script.js
+++ themeClock/src/script.js
@@ -1,1 +1,95 @@
-const title = document.querySelector('.title');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.seconde');
+const toggleBtn = document.querySelector('.btn-toggle-mode');
+const dateEL = document.querySelector('.date');
+const timeEl = document.querySelector('.time');
+
+const days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturdady',
+  'Sunday',
+];
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+toggleBtn.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.innerHTML = 'Dark Mode';
+  } else {
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light Mode';
+  }
+});
+
+function setTimeOfClock() {
+  const time = new Date();
+
+  const month = time.getMonth();
+  const hours = time.getHours();
+  const hourForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const date = time.getDate();
+  const day = time.getDay();
+
+  const ampm = hourForClock > 12 ? 'PM' : 'AM';
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hourForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.innerHTML = `${hourForClock}: ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  dateEL.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setTimeOfClock();
+
+setInterval(setTimeOfClock, 1000);
