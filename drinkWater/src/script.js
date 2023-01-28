e63ee2519aa154eb6c04239df0a0a6344e01d1a1
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-01-28 11:10:05 -0300

    feat: drink water app

---
+++ drinkWater/src/script.js
@@ -0,0 +1,53 @@
+const smallCups = document.querySelectorAll('.small-cup');
+const remained = document.querySelector('.remained');
+const liters = document.querySelector('.liters');
+const percentage = document.querySelector('.percentage');
+
+updateBigCup();
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => highLightCups(index));
+});
+
+function highLightCups(index) {
+  if (
+    smallCups[index].classList.contains('full') &&
+    !smallCups[index].nextElementSibling.classList.contains('full')
+  ) {
+    index--;
+  }
+
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.small-cup.full').length;
+
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
